2f22a170c039c031b9ff6126f174813d3e10e571
plzhang <plzhang2013@gmail.com>
2017-02-03 17:52:35 -0500

    1. Added resizing on width and height 2. Changed the batch size to 2 3. Added a timeline

--- models/baseline_cnn.py
+++ models/baseline_cnn.py
@@ -1,8 +1,13 @@
+from __future__ import print_function
 import numpy as np
 import dicom
 import os
-# import cv2
+import sys
+
+sys.path.append('/usr/local/lib/python2.7/site-packages')
+import cv2
 import tensorflow as tf
+from tensorflow.python.client import timeline
 
 
 def get_3d_data_from_dicom(path):
@@ -15,56 +20,55 @@ def get_3d_data_from_dicom(path):
   return np.stack([s.pixel_array for s in slices])
 
 
-def preprocess(data, depth, height, width):
+def preprocess(scan, depth, height, width):
   """
     Preprocess a single 3d data
     1. Resize to a uniform size
     2. ...
 
   """
-  if data.shape[0] > depth:
-    data = data[:depth]
+
+  # change depth.
+  if scan.shape[0] > depth:
+    scan = scan[:depth]
   else:
-    data = data
-    # TODO: fill with zeros.
+    scan = scan
+    # TODO: fill with air
+
+  # resize width and height
+  scan = np.array([cv2.resize(single_slice, (height, width)) for single_slice in scan])
 
   # This expansion add channel dimension as last dimension
-  data = np.expand_dims(data, axis=len(data.shape))
-  return data
+  scan = np.expand_dims(scan, axis=len(scan.shape))
+  return scan
 
 
-def conv3d(data, w):
-  return tf.nn.conv3d(data, w, strides=[1, 1, 1, 1, 1], padding='SAME')
+def conv3d(input_data, w, stride):
+  return tf.nn.conv3d(input_data, w, strides=stride, padding='SAME')
 
 
-def max_pool3d(data, depth_stride):
-  return tf.nn.max_pool3d(data, [1, depth_stride, 2, 2, 1], [1, depth_stride, 2, 2, 1], padding='SAME')
+def max_pool3d(input_data, depth_stride):
+  return tf.nn.max_pool3d(input_data, [1, depth_stride, 2, 2, 1], [1, depth_stride, 2, 2, 1], padding='SAME')
 
 
 # Parameters
-batch_size = 1
+batch_size = 2
 num_labels = 1
 
 in_depth = 120
-in_height = 512
-in_width = 512
+in_height = 128
+in_width = 128
 in_channels = 1
 
-filter_depth = 3
-filter_height = 3
-filter_width = 3
+filter_depth = 5
+filter_height = 5
+filter_width = 5
+conv_stride = [1, 1, 1, 1, 1]
 
-layer1_channels = 8
-layer2_channels = 8
+layer1_channels = 2
+layer2_channels = 2
 
 num_hidden = 64
-
-
-
-def conv_layer:
-
-
-
 
 # Graph
 
@@ -76,8 +80,6 @@ with tf.device('/cpu:0'):
   tf_train_dataset = tf.placeholder(
     tf.float32, shape=(batch_size, in_depth, in_height, in_width, in_channels))
   tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
-
-
 
   # Variables.
   layer1_weights = tf.Variable(tf.truncated_normal(
@@ -99,12 +101,13 @@ with tf.device('/cpu:0'):
 
   # Model.
   def model(data):
-    conv1 = conv3d(data, layer1_weights)
-    conv1 = tf.Print(conv1, [tf.argmax(conv1, 1)], 'argmax after conv1 = ')  # print something with tf.Print
+    conv1 = conv3d(data, layer1_weights, conv_stride)
+    # conv1 = tf.Print(conv1, [tf.argmax(conv1, 1)], 'After conv1 = ')  # print something with tf.Print
     conv1 = tf.nn.relu(conv1 + layer1_biases)
     pool1 = max_pool3d(conv1, 2)
 
-    conv2 = conv3d(pool1, layer2_weights)
+    conv2 = conv3d(pool1, layer2_weights, conv_stride)
+    # conv2 = tf.Print(conv2, [tf.argmax(conv2, 1)], 'After conv2 = ')  # print something with tf.Print
     conv2 = tf.nn.relu(conv2 + layer2_biases)
     pool2 = max_pool3d(conv2, 2)
 
@@ -112,6 +115,7 @@ with tf.device('/cpu:0'):
     reshape = tf.reshape(pool2, [shape[0], shape[1] * shape[2] * shape[3] * shape[4]])
 
     hidden = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
+    # hidden = tf.Print(hidden, [tf.argmax(hidden, 1)], 'After hidden layer = ')  # print something with tf.Print
     return tf.matmul(hidden, layer4_weights) + layer4_biases
 
 
@@ -126,20 +130,37 @@ with tf.device('/cpu:0'):
   # Prediction
   train_prediction = tf.nn.softmax(logits)
 
-num_steps = 10
+num_steps = 1
 
 # with tf.Session(graph=graph) as session:
-with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as session:
+with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as session:
+  run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+  run_metadata = tf.RunMetadata()
+
   tf.global_variables_initializer().run()
   print('Initialized')
 
-  # test on one data point
-  data = get_3d_data_from_dicom('../data/sample/images/0a0c32c9e08cc2ea76a71649de56be6d')
-  train_data = preprocess(data, in_depth, in_height, in_width)
-  print("preprocessed")
-  train_label = np.array([1])
+  # test on two data point in one batch
+  base_path = '../data/sample/images/'
+  paths = [base_path + '0a0c32c9e08cc2ea76a71649de56be6d',
+           base_path + '0c60f4b87afcb3e2dfa65abbbf3ef2f9']
+  data = [get_3d_data_from_dicom(path) for path in paths]
+
+  train_data = np.array([preprocess(single_slice, in_depth, in_height, in_width) for single_slice in data])
+  train_label = np.array([[1], [1]])
+
+  print('Preprocessed. Shape of training data: {0}'.format(train_data.shape))
 
   for step in range(num_steps):
-    feed_dict = {tf_train_dataset: np.array([train_data]), tf_train_labels: np.array([train_label])}
-    _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+    feed_dict = {tf_train_dataset: np.array(train_data), tf_train_labels: np.array(train_label)}
+    _, l, predictions = session.run([optimizer, loss, train_prediction],
+                                    feed_dict=feed_dict, options=run_options, run_metadata=run_metadata)
+
     print('Minibatch loss at step %d: %f' % (step, l))
+
+    # Create the Timeline object, and write it to a json
+    # go to the page 'chrome://tracing' and load the timeline.json file
+    tl = timeline.Timeline(run_metadata.step_stats)
+    ctf = tl.generate_chrome_trace_format()
+    with open('timeline.json', 'w') as f:
+      f.write(ctf)
