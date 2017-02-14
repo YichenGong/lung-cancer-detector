c661fc8756f0b007b778ac17194b598c580829f7
Chirag Maheshwari <cm4407@nyu.edu>
2017-02-14 12:02:24 -0500

    Minor Fixes to get the model running

--- multi-instance.py
+++ multi-instance.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import csv
 import numpy as np
 import tensorflow as tf
@@ -12,11 +11,20 @@ flags = tf.app.flags
 flags.DEFINE_integer("width", 128, "width")
 flags.DEFINE_integer("height", 128, "height")
 flags.DEFINE_integer("layers", 128, "layers")
-flags.DEFINE_integer("batch_size", 10, "batch size")
+flags.DEFINE_integer("batch_size", 3, "batch size")
 flags.DEFINE_integer("num_process", 1, "process number")
 flags.DEFINE_bool("is_train", True, "is train")
 flags.DEFINE_string("data_type", "stage1", "sample or stage1")
 config = flags.FLAGS
+
+def expand_last_dim(*input_data):
+  res = []
+  for in_data in input_data:
+    res.append(np.expand_dims(in_data, axis=len(in_data.shape)))
+  if len(res) == 1:
+    return res[0]
+  else:
+    return res
 
 def Weight(shape, name):
     return tf.Variable(name=name + "_Weights", 
@@ -39,7 +47,7 @@ def pool(x, name):
                          name=name + "_MaxPool")
 
 # Parameters
-batchSize = 10
+batchSize = 3
 
 imageSize = (128, 128, 128)
 labelsSize = 1
@@ -51,7 +59,7 @@ labelsInput = tf.placeholder(shape=[batchSize, labelsSize],
                             dtype=tf.float32,
                             name="InputLabels")
 
-imagesPlaceholder = tf.placeholder(shape=[batchSize, imageSize[0], imageSize[1], imageSize[2]],
+imagesPlaceholder = tf.placeholder(shape=[batchSize, imageSize[0], imageSize[1], imageSize[2], 1],
                                   dtype=tf.float32,
                                   name="InputImages")
 
@@ -59,22 +67,22 @@ imagesPlaceholder = tf.placeholder(shape=[batchSize, imageSize[0], imageSize[1],
 x_images = tf.reshape(imagesPlaceholder, [-1, imageSize[1], imageSize[2], 1])
 
 #convolution Layes
-hidden_Conv1 = tf.nn.relu(conv(x_images, [3, 3, 1, 16], "hidden_Conv1"))
+hidden_Conv1 = tf.nn.relu(conv(x_images, [3, 3, 1, 4], "hidden_Conv1"))
 hidden_Pool1 = pool(hidden_Conv1, "hidden_Conv1")
 
-hidden_Conv2 = tf.nn.relu(conv(hidden_Pool1, [3, 3, 16, 16], "hidden_Conv2"))
+hidden_Conv2 = tf.nn.relu(conv(hidden_Pool1, [3, 3, 4, 4], "hidden_Conv2"))
 hidden_Pool2 = pool(hidden_Conv2, "hidden_Conv2")
 
-hidden_Conv3 = tf.nn.relu(conv(hidden_Pool2, [3, 3, 16, 32], "hidden_Conv3"))
+hidden_Conv3 = tf.nn.relu(conv(hidden_Pool2, [3, 3, 4, 8], "hidden_Conv3"))
 hidden_Pool3 = pool(hidden_Conv3, "hidden_Conv2")
 
-hidden_Conv4 = tf.nn.relu(conv(hidden_Pool3, [3, 3, 32, 32], "hidden_Conv4"))
+hidden_Conv4 = tf.nn.relu(conv(hidden_Pool3, [3, 3, 8, 8], "hidden_Conv4"))
 hidden_Pool4 = pool(hidden_Conv4, "hidden_Conv4")
 
-hidden_Conv5 = tf.nn.relu(conv(hidden_Pool4, [3, 3, 32, 64], "hidden_Conv5"))
+hidden_Conv5 = tf.nn.relu(conv(hidden_Pool4, [3, 3, 8, 16], "hidden_Conv5"))
 hidden_Pool5 = pool(hidden_Conv5, "hidden_Conv5")
 
-hidden_Conv6 = tf.nn.relu(conv(hidden_Pool5, [3, 3, 64, 64], "hidden_Conv6"))
+hidden_Conv6 = tf.nn.relu(conv(hidden_Pool5, [3, 3, 16, 16], "hidden_Conv6"))
 hidden_Pool6 = pool(hidden_Conv6, "hidden_Conv6")
 
 flattened_vector = tf.reshape(hidden_Pool6, shape=[hidden_Pool6.get_shape()[0].value, 
@@ -107,7 +115,7 @@ prediction = tf.sigmoid(output)
 
 
 # Training
-num_epochs = 200
+num_epochs = 50
 sess_config = tf.ConfigProto()
 sess_config.gpu_options.allow_growth = True
 sess_config.log_device_placement=True
@@ -125,7 +133,7 @@ with tf.Session(config=sess_config) as session:
       train_data, train_label, _ = data_loader.next_batch()
       train_data, train_label = expand_last_dim(train_data, train_label)
 
-      feed_dict = {tf_dataset: train_data, tf_labels: train_label}
+      feed_dict = {imagesPlaceholder: train_data, labelsInput: train_label}
       _, l, preds = session.run([train_op, loss, prediction], feed_dict=feed_dict)
       print('labels: preds \n %s' % np.concatenate((train_label, preds), axis=1))
       print('Mini-batch loss: %f' % l)
@@ -139,7 +147,7 @@ with tf.Session(config=sess_config) as session:
       valid_data, valid_label, _ = data_loader.next_batch()
       valid_data, valid_label = expand_last_dim(valid_data, valid_label)
 
-      feed_dict = {tf_dataset: valid_data, tf_labels: valid_label}
+      feed_dict = {imagesPlaceholder: valid_data, labelsInput: valid_label}
       l = session.run(loss, feed_dict=feed_dict)
       batch_size = valid_data.shape[0]
       total_loss = total_loss + l * batch_size
@@ -156,14 +164,14 @@ with tf.Session(config=sess_config) as session:
     test_data, _, test_id = data_loader.next_batch()
     test_data = expand_last_dim(test_data)
     
-    feed_dict = {tf_dataset : test_data}
+    feed_dict = {imagesPlaceholder : test_data}
     preds = session.run(prediction, feed_dict=feed_dict)
     for i in range(test_data.shape[0]):
       pred_dict[test_id[i]] = preds[i][0]
 
   # TODO: write the predictions to file.
   print("Now update csv")
-  with open('submission_backup.csv', 'w') as f:
+  with open('submission_multi_instance_3.csv', 'w') as f:
     writer = csv.writer(f)
     # write the header
     for row in {'id':'cancer'}.items():
